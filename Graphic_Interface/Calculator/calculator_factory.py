1901bf34627c5b9e4f8eb7719e69336e3dc9c171
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-06-29 08:27:44 -0300

    Calculator project using Tkinter

--- Graphic_Interface/Calculator/calculator_factory.py
+++ Graphic_Interface/Calculator/calculator_factory.py
@@ -2,11 +2,18 @@ import tkinter as tk
 
 def criar_janelinha() -> tk.Tk:
     root = tk.Tk()
-    root.title("Calculator")
-    root.config(padx=10, pady=10, background='#000') # Vai configurar o espaçamento
-    root.resizable(False, False)
-    root.mainloop
+    root.title("Dom's Calculator")
+    root.config(background='#000', padx=10, pady=10)
+    root.resizable(False)
     return root
 
-def make_label(root) -> tk.Label: # Essa função servirá para definir a mensagem que aparece no topo
-    label = tk.Label(root, text='')
+def make_label(root) -> tk.Label: # Essa função precisa receber um parâmetro da função da janela (creio eu que todas as funções)
+    label = tk.Label(root, text='Sem texto ainda', anchor='e', justify='right')
+    # Criar grid para a mensagem
+    label.grid(row='0', column='0', columnspan='5', sticky='news')
+    return label # Sempre retornar a variável no final da função
+
+def make_display(root) -> tk.Entry:
+    display = tk.Entry(root)
+    display.grid(row='1', column='0' , columnspan='5', sticky='news', pady=(0, 10))
+    display.config(font=('TimesNewRoman', 20, 'bold'), justify='right', bd=1, relief='flat',)
--- Graphic_Interface/Calculator/main.py
+++ Graphic_Interface/Calculator/main.py
@@ -1,8 +1,9 @@
 import tkinter as tk
-from calculator_factory import criar_janelinha
+from calculator_factory import *
 
 def main():
     root = criar_janelinha()
+    label = make_label()
     root.mainloop()
     
 if __name__ == "__main__": # Precisa comparar para executar corretamente
