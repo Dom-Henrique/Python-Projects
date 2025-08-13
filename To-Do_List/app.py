dd188981c06f8fe670283c1596f9b33a1a645be3
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-08-13 13:44:35 -0300

    Main file of application, where will called the functions of created class

--- To-Do_List/app.py
+++ To-Do_List/app.py
@@ -1,2 +1,25 @@
 # Create a "To Do List"
 
+from userLogin import *
+from userRegister import *
+from taskRegister import *
+
+# Apresentação do sistema
+print('*'*50)
+print('Program initializated')
+print('*'*50)
+
+print('Welcome!')
+
+print('Please, log in the sistem or register new user')
+while True:
+    print('#'*50)
+    decision = int(input('1 - Login\t2 - User Register\n'))
+    if decision == 1:
+        userLogin()
+        break
+    elif decision == 2:
+        userRegister()
+        break
+    else:
+        print('Please, try again\n')
