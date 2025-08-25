369e2af84556223c53147f5c1950a71ef8369df7
Dom Henrique <127351728+Dom-Henrique@users.noreply.github.com>
2025-08-25 11:12:08 -0300

    Delete To-Do_List directory

--- To-Do_List/datetimeTest.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-from datetime import timedelta
-
-datinha = dt.datetime(day = 12, month = 12, year = 1912)
-lista = []
-
-datinha = datinha.strftime(f'%d/%m/%Y')
-
-print(datinha)
