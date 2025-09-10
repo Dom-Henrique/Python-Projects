74a7ec59f913a48ee2e7c6f944ed49f181978f30
Dom-Henrique <joaohenriqueds24@gmail.com>
2025-09-10 08:53:40 -0300

    Functions file of application. Here was created functions to register ride, list all availble rides and search ride by origin and destiny.

--- Ride-Software/V2/functions_v2.py
+++ Ride-Software/V2/functions_v2.py
@@ -1,49 +1,30 @@
-"""# Resto das funções
-from reg_log_v2 import UserLogin
-riders_rides = {'Rider Name': [], 'Rider E-mail': [], 'Origin': [], 'Destiny': [], 'Date': [], 'Time': [], 'Vacancies': [], 'Vacancies Price': []}
+from reg_log_v2 import *
 
-def RideRegister(origin, destiny, date, time, vacancies, vac_values, UserLogin):
-    riders_rides['Rider Name'].append(UserLogin.username)
-    riders_rides['Origin'].append(origin)
-    riders_rides['Destiny'].append(destiny)
-    riders_rides['Date'].append(date)
-    riders_rides['Time'].append(time)
-    riders_rides['Vacancies'].append(vacancies)
-    riders_rides['Vacancies Price'].append(vac_values)
-    print('Ride registred!')
-
-def ListRides():
-    for vagas_disponiveis in riders_rides['Vacancies']:
-        if vagas_disponiveis != 0:
-            print(f'Vacancies:\n{riders_rides["Vacancies"]}')
-            
-def Og_Dt_ListRides(origin, destiny):
-    for origem in riders_rides['Origin']:
-        if origem == origin:
-            for destino in riders_rides['Destiny']:
-                if destino == destiny:
-                    print(f'Founded vacancies:\nOrigin: {riders_rides["Origin"]}\tDestiny: {riders_rides["Destiny"]}')
-                    
-def ReserveVac(UserLogin.username, UserLogin.useremail):
-    index_user = riders_rides['Rider Name'].index(UserLogin.username)
-    wished_vac = riders_rides['Vacancies'].index(index_user)
-    vac_reserv = int(input('You want reserve vacancie?\n1 - S\t2 - N\n'))
+def Reg_Ride(origin, destiny, ride_date, ride_time, vacancies, value_vacancy, rides_data, username):
+    rides_data['Origin'].append(origin)
+    rides_data['Destiny'].append(destiny)
+    rides_data['Date of Ride'].append(ride_date)
+    rides_data['Time of Ride'].append(ride_time)
+    rides_data['Vacancies'].append(vacancies)
+    rides_data['Value per Vacancy'].append(value_vacancy)
     
-    if vac_reserv == 1:
-        riders_rides['Vacancies'][wished_vac] -= 1
-        print(f'Vacancie reserved!')
-        if riders_rides['Vacancies'][wished_vac] == 0:
-            print(f'Sorry! Cheio!')
-            
-def CancelVac(UserLogin.username, UserLogin.useremail):
-    index_user = riders_rides['Rider Name'].index(UserLogin.username)
-    wished_vac = riders_rides['Vacancies'].index(index_user)
-    vac_reserv = int(input('You want cancel the vacancie reserved?\n1 - S\t2 - N\n'))
+    print('Sucessful register!')
+    print(f'Congratulations, {username}! You registred a ride!')
     
-    if vac_reserv == 1:
-        riders_rides['Vacancies'][wished_vac] += 1
-        print(f'Vacancie reserve canceled!')
-        if riders_rides['Vacancies'][wished_vac] == 0:
-            print(f'Sorry! Cheio!')
-            
-def RemoveVac(RideRegister):"""
+def List_All_Rides(user_data, rides_data):
+    print('ALL RIDES AVAILBLE')
+    print(f'Rider Name: {user_data['Username']}')
+    print(f'Origin: {rides_data['Origin']}')
+    print(f'Destiny: {rides_data['Destiny']}')
+    print(f'Date of Ride: {rides_data['Date of Ride']}')
+    print(f'Time of Ride: {rides_data['Time of Ride']}')
+    print(f'Vacancies: {rides_data['Vacancies']}')
+    print(f'Value per Vacancy: {rides_data['Value per Vacancy']}')
+    
+def Search_by_Origin_Destiny(origin, destiny, rides_data):
+    for origin_search in rides_data['Origin']:
+        if origin == origin_search:
+            for destiny_search in rides_data['Destiny']:
+                if destiny == destiny_search:
+                    print(f'Origin: {origin}')
+                    print(f'')
